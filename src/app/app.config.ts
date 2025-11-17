7adbf3e40b1d0b3cf71c474bc1c965e2f0fb3c92
ketillybianca35 <ketillybianca35@gmail.com>
2025-11-17 18:26:22 -0300

    preparando para atualizar angular

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,22 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient } from '@angular/common/http';
-
 import { routes } from './app.routes';
+
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+
+import { environment } from './environments/environment';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideHttpClient()
+    provideHttpClient(),
+
+    // Firebase (correto)
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore())
   ]
 };
---
+++ src/app/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    apiKey: "AIzaSyDoGNPp61prMer5qFLh5OrnsNm_8-wO87c",
+    authDomain: "projeto-lumiere2025.firebaseapp.com",
+    projectId: "projeto-lumiere2025",
+    storageBucket: "projeto-lumiere2025.firebasestorage.app",
+    messagingSenderId: "928215513806",
+    appId: "1:928215513806:web:29e7421703a20223d76e9f",
+    measurementId: "G-5YH2DY9K7K"
+  }
+};
